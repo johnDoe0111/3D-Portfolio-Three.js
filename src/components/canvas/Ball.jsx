620d4405803856c8d1b6e3fd770e098e2ed14a22
johnDoe0111 <johndoe011@mail.ru>
2023-10-31 11:43:01 +0300

    WIP

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BallCanvas = () => {
+  return <div>Ball</div>;
+};
+
+export default BallCanvas;
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import About from './About';
+import Contact from './Contact';
+import Experience from './Experience';
+import Feedbacks from './Feedbacks';
+import Hero from './Hero';
+import CanvasLoader from './Loader';
+import Navbar from './Navbar';
+import Tech from './Tech';
+import Works from './Works';
+import {
+  BallCanvas,
+  ComputersCanvas,
+  EarthCanvas,
+  StarsCanvas,
+} from './canvas';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+  CanvasLoader,
+};
